de482564e00eea211748661d8beaef211cfb5cdc
Prelang Builder <builder@prelang.com>
2015-04-03 08:59:24 +0000

    Generated scaffolding for model 'Resume' via Rails

---
+++ app/views/resumes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @resume, :id, :name, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :resumes
+
   resources :educations
 
   resources :experiences

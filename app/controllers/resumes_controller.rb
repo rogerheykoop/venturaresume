818ad024e1695d291d52ded7f59fdb450895848a
Prelang Builder <builder@prelang.com>
2015-04-03 08:59:31 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -3,7 +3,7 @@ class ResumesController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_resume, only: [:show, :edit, :update, :destroy]
+  before_action :set_resume, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /resumes
   # GET /resumes.json

03c41b1f8a56586ad7735be9aa4bae7962e4a7ae
Prelang Builder <builder@prelang.com>
2015-04-03 08:59:24 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,4 +1,8 @@
 class ResumesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_resume, only: [:show, :edit, :update, :destroy]
 
   # GET /resumes
